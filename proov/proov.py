5f7808ae3eff574090cdb58a6627383c16b33239
annalukinjuk <opilane@HP-CZC6297BBM>
2021-10-29 09:09:21 +0300

    käärid

--- proov/proov.py
+++ proov/proov.py
@@ -1,20 +1,53 @@
-print("камень ножницы бумага")
+print("kivi käärid paber")
 import random
 a=0
 while(a==0):
-        player = int(input("1 - камень, 2 - ножницы, 3 - бумага. "))
-        if (player == 1 or player == 2 or player == 3):
-            ver = 1    
-if player == 1:
-        print("камень")  
-if player == 2:
-        print("ножницы") 
-if player == 3:
-        print("бумагa")  
-b = random.randint(1, 3)
-if b== 1:
-        print("камень.") 
-if b== 2:
-        print("ножницы")
-if b== 3:
-        print("бумагa")
+        mang=int(input("1-kivi,2-käärid,3-paber"))
+        if (mang==1 or mang==2 or mang==3):
+            a=1
+if mang==1:
+        print("kivi")
+if mang==2:
+        print("käärid")
+if mang==3:
+        print("paber")
+b=random.randint(1, 3)
+if  b==1:
+        print("kivi")
+if b==2:
+        print("käärid")
+if b==3:
+        print("paber")
+win=1
+win=2
+win=0
+if mang==1 and b==2:
+        win=1
+        if win==0:
+            print("withdraw")
+if win==1:
+        print("sa võinud")
+if win==2:
+        print("sa ei võinud")
+
+from keyboard import* 
+print("kivi käärid paber")
+while True:
+    try:
+        if read_key()== "k":
+            print("oli valitud kivi")
+            break
+    except:
+    ValueError
+m=3
+while m not in [1,2]:
+    try:
+        m=int(input("kellega mängime?\n1-inimestega\n2-roobotiga"))
+    except:
+        ValueError
+if m==1:
+    while 1:
+        pass
+elif m==2:
+    while 1:
+        pass
